fd0c7340cf446e38400fda3aadfb3dcdc24b7f76
serhatprogramming <serhatprogramming@gmail.com>
2023-09-03 21:59:52 -0400

    app refactor

--- src/App.js
+++ src/App.js
@@ -1,176 +1,27 @@
-import { useState, useEffect } from "react";
-import Playlist from "./components/Playlist";
+import { useEffect } from "react";
 import Notification from "./components/Notification";
-import Section from "./components/Section";
+import LoginForm from "./components/LoginForm";
+import PlaylistForm from "./components/PlaylistForm";
+import Playlists from "./components/Playlists";
 import "./App.css";
 
 import { useDispatch, useSelector } from "react-redux";
-
-import {
-  localStorageUser,
-  loginUser,
-  logoutUser,
-} from "./reducers/loginReducer";
-
-import {
-  fetchPlaylists,
-  likePlaylistAction,
-  deletePlaylistAction,
-  createPlaylistAction,
-} from "./reducers/playlistReducer";
+import { localStorageUser } from "./reducers/loginReducer";
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [playlistName, setPlaylistName] = useState("");
-  const [creator, setCreator] = useState("");
-  const [numOfSongs, setNumOfSongs] = useState("");
-  const [likes, setLikes] = useState("");
-
   const dispatch = useDispatch();
   const loggedUser = useSelector((state) => state.loggedUser);
-  const playlists = useSelector((state) => state.playlists);
   const notification = useSelector((state) => state.notification);
-
-  useEffect(() => {
-    dispatch(fetchPlaylists());
-  }, []);
 
   useEffect(() => {
     dispatch(localStorageUser());
   }, []);
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    dispatch(loginUser({ username, password }));
-    setUsername("");
-    setPassword("");
-  };
-
-  const handleLogout = () => {
-    dispatch(logoutUser());
-  };
-
-  const handleAddPlaylist = async (event) => {
-    event.preventDefault();
-    dispatch(
-      createPlaylistAction({
-        name: playlistName,
-        creator,
-        numOfSongs: numOfSongs === "" || numOfSongs < 0 ? 0 : numOfSongs,
-        likes: likes === "" || likes < 0 ? 0 : likes,
-        user: loggedUser,
-      })
-    );
-    setPlaylistName("");
-    setCreator("");
-    setNumOfSongs("");
-    setLikes("");
-  };
-
-  const handleLike = (id, likes) => {
-    dispatch(likePlaylistAction(id, likes));
-  };
-
-  const handleRemove = async (id) => {
-    dispatch(deletePlaylistAction(id, loggedUser));
-  };
-
-  const userLoginForm = () => (
-    <form onSubmit={handleLogin}>
-      <h3>Log in to playlist application</h3>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
-
-  const displayPlaylists = () => (
-    <>
-      <h2>Playlist Application</h2>
-      <em>Howdy, {loggedUser.username}! </em>
-      <button onClick={handleLogout}>Log Out</button>
-      <h3>Playlists</h3>
-      {playlists.map((playlist) => (
-        <Section
-          componentTitle={playlist.name + " by " + playlist.creator}
-          key={playlist.id}
-        >
-          <Playlist
-            playlist={playlist}
-            handleLike={handleLike}
-            handleRemove={handleRemove}
-            username={loggedUser.username}
-          />
-        </Section>
-      ))}
-    </>
-  );
-
-  const addPlaylistForm = () => (
-    <form onSubmit={handleAddPlaylist}>
-      <h3>Add Playlists</h3>
-      <div>
-        Playlist Name:
-        <input
-          type="text"
-          value={playlistName}
-          onChange={(e) => setPlaylistName(e.target.value)}
-          data-testid="playlist-name"
-        />
-      </div>
-      <div>
-        Creator:
-        <input
-          type="text"
-          value={creator}
-          onChange={(e) => setCreator(e.target.value)}
-          data-testid="creator"
-        />
-      </div>
-      <div>
-        Number of Songs:
-        <input
-          type="number"
-          value={numOfSongs}
-          onChange={(e) => setNumOfSongs(e.target.value)}
-          data-testid="songs"
-        />
-      </div>
-      <div>
-        Likes:
-        <input
-          type="number"
-          value={likes}
-          onChange={(e) => setLikes(e.target.value)}
-          data-testid="likes"
-        />
-      </div>
-      <button data-testid="add-playlist-button" type="submit">
-        Add Playlist
-      </button>
-    </form>
-  );
-
   return (
     <div>
       {notification && <Notification />}
-      {loggedUser ? displayPlaylists() : userLoginForm()}
-      {loggedUser && addPlaylistForm()}
+      {loggedUser ? <Playlists /> : <LoginForm />}
+      {loggedUser && <PlaylistForm />}
     </div>
   );
 };
---
+++ src/components/PlaylistForm.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { createPlaylistAction } from "../reducers/playlistReducer";
+import { useSelector, useDispatch } from "react-redux";
+
+const PlaylistForm = () => {
+  const [playlistName, setPlaylistName] = useState("");
+  const [creator, setCreator] = useState("");
+  const [numOfSongs, setNumOfSongs] = useState("");
+  const [likes, setLikes] = useState("");
+
+  const dispatch = useDispatch();
+  const loggedUser = useSelector((state) => state.loggedUser);
+
+  const handleAddPlaylist = async (event) => {
+    event.preventDefault();
+    dispatch(
+      createPlaylistAction({
+        name: playlistName,
+        creator,
+        numOfSongs: numOfSongs === "" || numOfSongs < 0 ? 0 : numOfSongs,
+        likes: likes === "" || likes < 0 ? 0 : likes,
+        user: loggedUser,
+      })
+    );
+    setPlaylistName("");
+    setCreator("");
+    setNumOfSongs("");
+    setLikes("");
+  };
+
+  return (
+    <form onSubmit={handleAddPlaylist}>
+      <h3>Add Playlists</h3>
+      <div>
+        Playlist Name:
+        <input
+          type="text"
+          value={playlistName}
+          onChange={(e) => setPlaylistName(e.target.value)}
+          data-testid="playlist-name"
+        />
+      </div>
+      <div>
+        Creator:
+        <input
+          type="text"
+          value={creator}
+          onChange={(e) => setCreator(e.target.value)}
+          data-testid="creator"
+        />
+      </div>
+      <div>
+        Number of Songs:
+        <input
+          type="number"
+          value={numOfSongs}
+          onChange={(e) => setNumOfSongs(e.target.value)}
+          data-testid="songs"
+        />
+      </div>
+      <div>
+        Likes:
+        <input
+          type="number"
+          value={likes}
+          onChange={(e) => setLikes(e.target.value)}
+          data-testid="likes"
+        />
+      </div>
+      <button data-testid="add-playlist-button" type="submit">
+        Add Playlist
+      </button>
+    </form>
+  );
+};
+
+export default PlaylistForm;
